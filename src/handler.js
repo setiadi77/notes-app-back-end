d35ce65e016dd06afc34fbd8a0254c4a5a2c24d9
Setiadi <setiadizainal12@gmail.com>
2022-04-03 13:44:01 +0700

    mengubah catatan
    add handler dan routes untuk mengubah catatan

--- src/handler.js
+++ src/handler.js
@@ -66,4 +66,40 @@ const getNoteByIdHandler = (request, h) => {
 
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const { title, tags, body } = request.payload;
+    const updateAt = new Date().toISOString();
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Catatan berhasil diperbaharui',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    });
+    return response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, 
+    getAllNotesHandler, 
+    getNoteByIdHandler,
+    editNoteByIdHandler, };
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler } = require("./handler");
 
 const routes = [
     {
@@ -16,6 +16,11 @@ const routes = [
         path: '/notes/{id}',
         handler: getNoteByIdHandler,
     },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    }
 ];
 
 module.exports = routes;
